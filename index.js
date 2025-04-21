13d7c6738d0f686c52b38697d4b3fd1f0410e600
neo050 <neoray.asking666@gmail.com>
2025-04-21 18:54:55 +0300

    new

--- index.js
+++ index.js
@@ -26,18 +26,27 @@ app.post("/get-weather",async(req,res)=>
             //{data:response.data}
         try
         {
-            console.log("yep");
             const response=await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${req.body.city}&limit=1&appid=${apikey}`);
-            console.log(response.data);
-            const response2=await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${response.data[0].lat}&lon=${response.data[0].lon}&appid=${apikey}`);
-            console.log("yeppppp:\n",response2.data);
-            res.render("index.ejs",{data:{description:response2.data.weather[0].description, temp: (response2.data.main.temp - 273.15).toFixed(2),feels_like: (response2.data.main.feels_like- 273.15).toFixed(2), temp_min:(response2.data.main.temp_min - 273.15).toFixed(2),temp_max:(response2.data.main.temp_max - 273.15).toFixed(2)}})
+            if(response.data.length>0)
+            {
+                console.log(response.data)
+                const response2=await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${response.data[0].lat}&lon=${response.data[0].lon}&appid=${apikey}`);
+                res.render("index.ejs",{data:{description:response2.data.weather[0].description, temp: (response2.data.main.temp - 273.15).toFixed(2),feels_like: (response2.data.main.feels_like- 273.15).toFixed(2), temp_min:(response2.data.main.temp_min - 273.15).toFixed(2),temp_max:(response2.data.main.temp_max - 273.15).toFixed(2)}})
+            }
+            else
+            {
+                res.render("index.ejs",{error:"We couldn’t find that location. Please check that you’ve entered the full and correct city and country names."})
+
+            }
+
+            
+        
 
            
         }
         catch(error)
         {
-            res.render("index.ejs",{error:JSON.stringify(error.response.data)})
+            res.render("index.ejs",{error:"Sorry, something went wrong while fetching the weather. Please try again in a moment."})
 
             console.log(JSON.stringify(error.response.data));
         }
